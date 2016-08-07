927c3c007f6e38cb6263f7dba44ac35aabc094a2
JoseluGames <joselugamesofficial@gmail.com>
2016-08-07 17:32:35 +0200

    Ok, now haha

--- src/main/java/com/jlgm/chatbox/event/ChatBoxEventHandler.java
+++ src/main/java/com/jlgm/chatbox/event/ChatBoxEventHandler.java
@@ -4,6 +4,7 @@ import com.jlgm.chatbox.main.ChatBoxMain;
 
 import net.minecraft.util.text.Style;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.event.ClickEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -14,10 +15,17 @@ public class ChatBoxEventHandler {
 	public void onPlayerTickEvent(PlayerTickEvent event){
 		if(!ChatBoxMain.haveWarnedVersionOutOfDate && event.player.worldObj.isRemote && !ChatBoxMain.versionChecker.isLatestVersion()){
 			ClickEvent versionCheckChatClickEvent = new ClickEvent(ClickEvent.Action.OPEN_URL, "https://minecraft.curseforge.com/projects/chatbox");
-			Style clickableChatStyle = new Style().setClickEvent(versionCheckChatClickEvent);
-			TextComponentString versionWarningChatComponent = new TextComponentString("Your ChatBox is no the latest version! Click here to update");
+			Style clickableChatStyle = new Style().setClickEvent(versionCheckChatClickEvent).setColor(TextFormatting.GREEN);
+			TextComponentString versionWarningChatComponent = new TextComponentString("ChatBox is outdated! Click here to update to ");
+			TextComponentString versionComponent = new TextComponentString(ChatBoxMain.versionChecker.getLatestVersionInfo().get(0));
+			versionComponent.setStyle(new Style().setColor(TextFormatting.RED).setBold(true).setUnderlined(true));
 			versionWarningChatComponent.setStyle(clickableChatStyle);
+			versionWarningChatComponent.appendSibling(versionComponent);
 			event.player.addChatMessage(versionWarningChatComponent);
+			event.player.addChatComponentMessage(new TextComponentString("Changelog: ").setStyle(new Style().setColor(TextFormatting.YELLOW)));
+			for(int i = 1; i < ChatBoxMain.versionChecker.getLatestVersionInfo().size(); i++){
+				event.player.addChatMessage(new TextComponentString("- " + ChatBoxMain.versionChecker.getLatestVersionInfo().get(i)).setStyle(new Style().setColor(TextFormatting.YELLOW)));
+			}
 			ChatBoxMain.haveWarnedVersionOutOfDate = true;
 		}
 	}
