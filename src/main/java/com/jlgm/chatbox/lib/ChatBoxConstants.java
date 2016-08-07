1543cabca935917bcf91df965ba3dd62228c044f
JoseluGames <joselugamesofficial@gmail.com>
2016-08-07 15:04:23 +0200

    Change model and texture for Chat Box
    1.10.2-0.2.0

--- src/main/java/com/jlgm/chatbox/lib/ChatBoxConstants.java
+++ src/main/java/com/jlgm/chatbox/lib/ChatBoxConstants.java
@@ -9,8 +9,8 @@ public class ChatBoxConstants {
 	//Version parts ( https://mcforge.readthedocs.io/en/latest/conventions/versioning/ )
 	public static final String MCVERSION = "1.10.2";
 	public static final String MAJOR = "0";
-	public static final String MINOR = "1";
-	public static final String PATCH = "1";
+	public static final String MINOR = "2";
+	public static final String PATCH = "0";
 	public static final String RELEASETYPE = "";
 	
 	//The version of the mod
